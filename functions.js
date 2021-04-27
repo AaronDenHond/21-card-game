fe522ab79b1df5c55df23a3a2ab98a186ea3aeb9
Aaron Den Hond <aaron.denhond@gmail.com>
2021-04-27 17:03:56 +0200

    update exercise Blackjack

--- functions.js
+++ functions.js
@@ -2,11 +2,12 @@ function getDeck() {
     let suits = ["spades", "diamonds", "clubs", "hearts"];   // for array deck
     let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]; // for array deck
     let deck = new Array();
+    let realValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
 
 
     for (let i = 0; i < suits.length; i++) {
         for (let x = 0; x < values.length; x++) {
-            let card = { value: values[x], suit: suits[i] };
+            let card = { value: values[x], suit: suits[i], realValue: realValues[x] };
             deck.push(card);
 
 
@@ -26,4 +27,29 @@ function drawCard() {
     return (card);
 
 
+}
+
+function showCards(deck, target) {
+    let returnString = ""; // per click return string leeggemaakt
+    let score = 0;
+    deck.forEach((element) => {
+        let cardText = element.suit + " " + element.value + " ";
+        returnString += cardText;
+        score += element.realValue;
+
+    });
+    returnString += " score is " + score;
+    target.innerHTML = returnString;
+}
+
+function calculateScore(deck) {
+
+    let score = 0;
+    deck.forEach((element) => {
+
+
+        score += element.realValue;
+
+    });
+    return score;
 }
--- script.js
+++ script.js
@@ -1,18 +1,52 @@
-let superDeck = getDeck();
+let superDeck = getDeck();  //declare variables only once, either globally or scoped
 let playerDeck = [];
 let playerTarget = document.getElementById("playerTarget");
-
+let cpuDeck = [];
+let cpuTarget = document.getElementById("cpuTarget");
 
 document.getElementById("draw").addEventListener("click", function () {
     playerDeck.push(drawCard());
     console.log(playerDeck);
     console.log(superDeck);
-    let returnString = ""; // per click return string leeggemaakt
-
-    playerDeck.forEach((element) => {
-        let cardText = element.suit + " " + element.value + " ";
-        returnString += cardText;
-    });
-    playerTarget.innerHTML = returnString;
+    showCards(playerDeck, playerTarget);
     //for each in playerdeck in player target
 });
+
+document.getElementById("playGame").addEventListener("click", function () {
+
+    //shuffle deck
+    // 2 cards each
+    //player input draw or hold
+    //show player cards
+    superDeck = getDeck();  //deck reset
+    playerDeck = [];  // array 
+    playerDeck.push(drawCard());
+    playerDeck.push(drawCard());
+    showCards(playerDeck, playerTarget);
+
+
+
+    cpuDeck = [];  // array 
+    cpuDeck.push(drawCard());
+    cpuDeck.push(drawCard());
+    showCards(cpuDeck, cpuTarget);
+});
+
+document.getElementById("hold").addEventListener("click", function (){
+let cpuscore = calculateScore(cpuDeck);
+let playerscore = calculateScore(playerDeck);
+if (cpuscore > playerscore){
+    alert("Game lost")
+} else if (cpuscore == 21) {
+    alert("Game lost")
+}
+  cpuDeck.push(drawCard());
+  showCards(cpuDeck, cpuTarget);
+  if (cpuscore > playerscore){
+    alert("Game lost")
+} else if (cpuscore == 21) {
+    alert("Game lost")
+}
+});
+
+//cpu slim laten redeneren, functies schrijven voor cpu
